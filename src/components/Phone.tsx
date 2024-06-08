9a36afc887cd4352fbf1ca76a7747da9e1744a55
Advay Gon Choudhury <adgoch11@gmail.com>
2024-06-09 00:06:35 +0530

    Update landing page 2

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -18,6 +18,24 @@ export default function Phone({
         "relative pointer-events-none z-50 overflow-hidden",
         className
       )}
-    ></div>
+      {...props}
+    >
+      <img
+        className="pointer-events-none z-50 select-none"
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        alt="phone image"
+      />
+      <div className="absolute -z-10 inset-0">
+        <img
+          className="object-cover"
+          src={imgSrc}
+          alt="overlaying phone image"
+        />
+      </div>
+    </div>
   );
 }
